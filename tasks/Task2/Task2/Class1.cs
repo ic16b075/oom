eaf1ea2b7aaec11a542d9cefc233f55b0b5db391
Eduard <ic16b075@technikum-wien.at>
2017-03-06 01:46:03 +0100

    add project Task2

---
+++ tasks/Task2/Task2/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Dvd
+    {
+        /* private fields */
+        private string title;
+        private double price;
+        private decimal release_year;
+
+        /* constructor */
+        public Dvd(string new_title, double new_price, decimal new_release_year)
+        {
+            if (string.IsNullOrWhiteSpace(new_title)) throw new ArgumentException("Title must not be empty!", nameof(new_title));
+
+            if (new_release_year < 1888) throw new ArgumentOutOfRangeException("Enter valid release year!");
+
+            Title = new_title;
+            Price = new_price;
+            Release_year = new_release_year;
+        }
+
+        /* public properties */
+        public string Title { get; }
+        public double Price { get;  }
+        public decimal Release_year { get; }
+
+
+        /* public method */
+        public void UpdatePrice(double updated_new_pice)
+        { /* update price with exception price < 0 */
+            if (updated_new_pice < 0) throw new ArgumentException("Enter non-negative price!", nameof(updated_new_pice));
+            price = updated_new_pice;
+        }
+    }
+}
---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class MainCLass
+    {
+        public static void Main(string[] args)
+        {
+            var dvds = new[]
+            {
+                new Dvd("Adams Aepfel", 5.90, 2005),
+                new Dvd("Headhunters", 9.90, 2011),
+                new Dvd("In China essen sie Hunde", 2.90, 1999),
+            };
+
+            foreach (var element in dvds)
+            {/*Price fixen in Klasse*/
+                Console.WriteLine(" {0} | {1} | {2} EUR", element.Title, element.Release_year, element.Price);
+            }
+            /*Price ändern*/
+            /*nochmal foreach ausgeben */
+        }
+    }
+}
